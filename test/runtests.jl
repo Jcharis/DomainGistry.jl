cba94f26efc59b7c4fb3614634d26ab73f8903ac
JCharis <jcharistech@gmail.com>
2019-09-14 19:05:08 +0300

    Domain Name Generation Package For Julia

---
+++ test/runtests.jl
@@ -0,0 +1,7 @@
+using Test
+using DomainGistry
+
+@testset "Domain Name Generation Fxn" begin
+    @test DomainGistry.generate("example") == ["example.com" ,"example.edu" ,"example.net" ,"example.org" ,"example.site","example.co"  ,"example.io"  ,"example.ai"  ,"example.app" ,"example.ca"  ,"example.uk"  ,"example.ua"  ,"example.us"  ,"example.ru"  ,"example.ch"]
+    @test DomainGistry.generate("example","common") == ["example.com" ,"example.edu" ,"example.net" ,"example.org" ,"example.site","example.co"  ,"example.io"  ,"example.ai"  ,"example.app" ,"example.ca"  ,"example.uk"  ,"example.ua"  ,"example.us"  ,"example.ru"  ,"example.ch"]
+end
